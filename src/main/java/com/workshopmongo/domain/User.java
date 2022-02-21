c8e6da2a034cf46db2a3dbe974730fc1b57b68f6
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-02-21 13:58:16 -0300

    Referenciando os posts do usuário

--- src/main/java/com/workshopmongo/domain/User.java
+++ src/main/java/com/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -14,6 +17,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User() {
     }
@@ -48,6 +54,10 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    public List<Post> getPosts() {
+        return posts;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -60,4 +70,6 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+
 }
